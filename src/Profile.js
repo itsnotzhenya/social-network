08339616c2cb4b01faad8abef89a2a63e6075894
e.shkriada <e.shkriada@intch.org>
2023-04-14 16:02:19 +0700

    Split code to separate components

---
+++ src/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Profile = () => {
+  return (
+    <div className="main">
+      <div>
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          alt="background"
+        />
+      </div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC4U0lJ-T1fI5y0Oee2mtbC2snM0EP1fhCN-I9kAIdaNjuHqGrG9MLdlAbfBTG8YsxiwI&usqp=CAU"
+        alt="avatar"
+      />
+    </div>
+  );
+};
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" />
+    </header>
+  );
+};
