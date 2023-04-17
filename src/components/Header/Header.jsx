ec4a3c3d8fb91e38532f606fbd993ed4c9851080
e.shkriada <e.shkriada@intch.org>
2023-04-17 15:27:31 +0700

    Move styles to css modules

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import s from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src={logo} alt="logo" />
+      <h1 className={s.main_title}>MySpace</h1>
+    </header>
+  );
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import styles from './Navbar.module.css'
 
 export const Navbar = () => {
   return (
-    <nav className="nav">
-      <div className='item'>
+    <nav className={styles.nav}>
+      <div className={styles.item}>
         <a href="#">Profile</a>
       </div>
-      <div className='item'>
+      <div className={styles.item}>
         <a href="#">Messages</a>
       </div>
-      <div className='item'>
+      <div className={styles.item}>
         <a href="#">News</a>
       </div>
-      <div className='item'>
+      <div className={styles.item}>
         <a href="#">Music</a>
       </div>
-      <div className='item'>
+      <div className={styles.item}>
         <a href="#">Settings</a>
       </div>
     </nav>
